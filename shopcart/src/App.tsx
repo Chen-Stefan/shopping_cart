399af434d8b03b19b00e01ff572aaca4434d955f
Stefan Chen <zchen158@my.bcit.ca>
2022-07-12 21:02:14 -0700

    Create three pages: home, store, about

--- shopcart/src/App.tsx
+++ shopcart/src/App.tsx
@@ -1,9 +1,12 @@
 import { Routes, Route} from 'react-router-dom'
 import { Container } from 'react-bootstrap'
+import { Home } from './pages/Home';
+import { Store } from './pages/Store';
+import { About } from './pages/About';
 
 function App() {
   return (
-    <Container>
+    <Container className='mb-4'>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/store' element={<Store />} />
